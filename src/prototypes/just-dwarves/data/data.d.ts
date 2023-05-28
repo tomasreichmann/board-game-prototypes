bfb77c6f8ef7e202ac2f8ec00e095c8f035b0662
Tomáš Reichmann <tomasreichmann@gmail.com>
2023-05-28 19:33:26 +0200

    data.d.ts fix

--- src/prototypes/just-dwarves/data/data.d.ts
+++ src/prototypes/just-dwarves/data/data.d.ts
@@ -1,3 +1,15 @@
-declare module "*.csv" {
+declare module "*buildings.csv" {
+    export default data as { [key: string] }[];
+}
+
+declare module "*clanChiefs.csv" {
+    export default data as { [key: string] }[];
+}
+
+declare module "*components.csv" {
+    export default data as { [key: string] }[];
+}
+
+declare module "*events.csv" {
     export default data as { [key: string] }[];
 }

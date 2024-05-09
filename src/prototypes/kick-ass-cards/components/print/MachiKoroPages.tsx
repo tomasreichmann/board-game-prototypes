7887270e15574e4d53c360ca665d57114ac26edb
Tomáš Reichmann <tomasreichmann@gmail.com>
2024-05-09 22:59:02 +0200

    Added machikoro tender cards (for real now)

--- src/prototypes/kick-ass-cards/components/print/MachiKoroPages.tsx
+++ src/prototypes/kick-ass-cards/components/print/MachiKoroPages.tsx
@@ -12,12 +12,82 @@ import BuildingCard, {
 
 const buildings: BuildingCardProps[] = [
     {
-        activation: "",
+        title: "Malý pohostinský řetězec",
+        activation: "3 x [food] (6-)",
         deckType: DeckTypeEnum.Small,
         type: BuildingTypeEnum.PublicTender,
         cost: 0,
+        reward: 20,
+        description:
+            "Na zakázku japonského konglomerátu vybuduj 3 [food] objekty s aktivační hodnotou (6-) a odevzdej je za odměnu 20 mincí.",
+    },
+    {
+        title: "Velký pohostinský řetězec",
+        activation: "4 x [food] (7+)",
+        deckType: DeckTypeEnum.Big,
+        type: BuildingTypeEnum.PublicTender,
+        cost: 0,
+        reward: 40,
+        description:
+            "Na zakázku japonského konglomerátu vybuduj 4 [food] objekty s aktivační hodnotou 7+ a odevzdej je za odměnu 40 mincí.",
+    },
+    {
+        title: "Sousedská výměna",
+        activation: "2 x [any]",
+        deckType: DeckTypeEnum.Landmark,
+        type: BuildingTypeEnum.PublicTender,
+        cost: 0,
         reward: 0,
-        description: "",
+        description:
+            "Bohatý soused nabízí výhodnou výměnu.\n2 z tvých libovolných podniků odevzdej a vezmi si 1 podnik z obecné nabídky.",
+    },
+    {
+        title: "Yakuza úřaduje",
+        activation: "1 x [special]",
+        deckType: DeckTypeEnum.Landmark,
+        type: BuildingTypeEnum.PublicTender,
+        cost: 0,
+        reward: 15,
+        description: "Má-li hráč na tahu alespoň 3 [special], může odevzdat jednu z nich Yakuze za odměnu 15 mincí.",
+    },
+    {
+        title: "Konbini nadšenec",
+        activation: "3 x Maloobchod",
+        deckType: DeckTypeEnum.Small,
+        type: BuildingTypeEnum.PublicTender,
+        cost: 0,
+        reward: 15,
+        description: "Jako fanatik do fami-chiki odevzdej 3 podniky Maloobchod za odměnu 15 mincí.",
+    },
+    {
+        title: "Malý pasivní příjem",
+        activation: "3 x [industry] (6-) / 3x [agriculture] (6-) / 3x [cow] (6-)",
+        deckType: DeckTypeEnum.Small,
+        type: BuildingTypeEnum.PublicTender,
+        cost: 0,
+        reward: 5,
+        description:
+            "Pokud má hráč na tahu 3+ kopií podniků typu [industry],  [agriculture] nebo [cow] s aktivační hodnotou 6-,  může je investovat a odevzdat výměnou za tuto kartu\n\nPasivní příjem\nVlastník této karty si na začátku každého kola vezme z banky 3 mince.",
+    },
+    {
+        title: "Velký pasivní příjem",
+        activation: "4 x [industry] (7+) / 4x [agriculture] (7+)",
+        deckType: DeckTypeEnum.Big,
+        type: BuildingTypeEnum.PublicTender,
+        cost: 0,
+        reward: 10,
+        description:
+            "Pokud má hráč na tahu 4+ kopií podniků typu [industry] nebo [agriculture] s aktivační hodnotou 7+,  může je investovat a odevzdat výměnou za tuto kartu\n\nPasivní příjem\nVlastník této karty si na začátku každého kola vezme z banky 10 mincí.",
+    },
+    {
+        title: "Rozvinutý dopravní sektor ",
+        activation: "",
+        deckType: DeckTypeEnum.Big,
+        type: BuildingTypeEnum.PublicTender,
+        cost: 0,
+        reward: 0,
+        description:
+            "Má-li hráč na tahu aktivovaný přístav, nádraží i letiště, může si vybrat a postavit 1 další [landmark] objekt.",
     },
 ];
 

49ec55bb75b294d5bc9a8cbb9a9de88e710bc97d
Tomáš Reichmann <t_reichmann@wargaming.net>
2024-09-06 19:36:35 +0200

    Though Shalt Not Prank update

--- src/prototypes/kick-ass-cards/components/content/Article.tsx
+++ src/prototypes/kick-ass-cards/components/content/Article.tsx
@@ -8,7 +8,20 @@ export default function Article({ className, children }: ArticleProps) {
     return (
         <article
             className={twMerge(
-                "prose text-kac-iron max-w-none prose-headings:font-kacHeading prose-headings:text-kac-blood prose-headings:leading-tight prose-headings:tracking-tighter prose-a:text-kac-monster prose-strong:text-kac-iron-dark prose-blockquote:text-kac-iron-dark prose-em:text-kac-cloth prose-ul:[--tw-prose-bullets:rgb(123,0,29)] prose-ul:my-0 prose-li:my-0",
+                "prose",
+                "text-kac-iron",
+                "max-w-none",
+                "prose-headings:font-kacHeading",
+                "prose-headings:text-kac-blood",
+                "prose-headings:leading-tight",
+                "prose-headings:tracking-tighter",
+                "prose-a:text-kac-monster",
+                "prose-strong:text-kac-iron-dark",
+                "prose-blockquote:text-kac-iron-dark",
+                "prose-em:text-kac-cloth-dark",
+                "prose-ul:[--tw-prose-bullets:rgb(123,0,29)]",
+                "prose-ul:my-0",
+                "prose-li:my-0",
                 className
             )}
         >
